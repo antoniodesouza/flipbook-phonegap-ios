d1c22e2bbf55fe142d9733d7e5579c0457045cce
Luis Alberto Castillo <castillolu@gmail.com>
2012-10-31 12:02:40 -0500

    Creación de utils.js para la organizacion de funciones JS y organización de CSS

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@
  * and open the template in the editor.
  */
 $(document).bind('pageinit',function(){
-    var slideWidth = $(window).width();
-    var slides = $('.slide');
-    var numberOfSlides = slides.length;
-    var position = 0;
+    slideWidth = $(window).width();
+    slides = $('.slide');
+    numberOfSlides = slides.length;
+    position = 0;
+    currentScroll = 0;
                 
-    manageControls();
-    $("#num-slide").html(position+1);
+    tools.manageControls();
+    tools.loadSlides();
                 
     // Wrap all .slides with #slideInner div
     slides.wrapAll('<div id="slideInner"></div>').css({
@@ -19,117 +20,13 @@ $(document).bind('pageinit',function(){
                 
     // Set #slideInner width equal to total width of all slides
     $('#slideInner').css('width', slideWidth * numberOfSlides);
-    $('#slideInner').css('height', 511);
-                
-    $(document).bind('vmouseup',evMouseUp);
-    $("#left").bind('tap', evLeft);
-    $("#right").bind('tap', evRight);
-    $("#contenido li").bind('tap', evContenido)
-    $("#btn-inicio").bind('tap', evInicio);
-                
-    var currentScroll = 0;
-    $(document).bind('vmousedown', function(e){
-        currentScroll = $("#slidesContainer").scrollLeft();
-    });
-                
-                            
-    $(document).bind('tap', function(){
-        $("#btn_menu").toggle();
-    });
-                
-    function manageControls(){
-        $("#left").css('opacity','1');
-        $("#right").css('opacity','1');
-                	
-        if (position == 0){
-            $("#left").css('opacity','0.5');
-        }
-        else if (position >= numberOfSlides-1){
-            $("#right").css('opacity','0.5');
-        }
-    }
-                
-    function evRight(){
-        if (position < numberOfSlides-1){
-            position++;
-            goToPage(position);
-        }
-    }
-                
-    function evLeft(){
-        if (position > 0){
-            position--;
-            goToPage(position);
-        }
-    }
-                
-    function evContenido(){
-        position = parseInt($(this).attr("rel"));
-        goToPage(position);
-        $("#contenido").hide();
-    }
-                
-    function evInicio(){
-        position = 0;
-        goToPage(position);
-    }
-                    
-    function evMouseUp(){
-        if ($("#slidesContainer").scrollLeft() > currentScroll){
-            var pos = ($("#slidesContainer").scrollLeft() + slideWidth)/slideWidth;
-        }else
-        if ($("#slidesContainer").scrollLeft() < currentScroll){
-            var pos = ($("#slidesContainer").scrollLeft() - slideWidth)/slideWidth;
-        }else{
-            return false;
-        }
-                    
-        if (pos < 0){
-            pos = 0;
-        }
-        if (pos > numberOfSlides-1){
-            pos = numberOfSlides-1;
-        }
-        position = Math.round(pos);
-        goToPage(position);
-             
-    }
-                
-                
-    function goToPage(page){
-        $(document).unbind('vmouseup');
-        $("#left").unbind('tap');
-        $("#right").unbind('tap');
-        $("#contenido li").unbind('tap');
-        $("#btn-inicio").unbind('tap');
-                
-        manageControls();
-        $("#slidesContainer").animate({
-            scrollLeft:slideWidth*page
-            },500,'easeOutQuint',function(){
-            $(document).bind('vmouseup',evMouseUp);
-            $("#left").bind('tap', evLeft);
-            $("#right").bind('tap', evRight);
-            $("#contenido li").bind('tap', evContenido)
-            $("#btn-inicio").bind('tap', evInicio);
-        });
-        $("#num-slide").html(page+1);
-    }
-                
-                
-    $("#btn-menu").bind('tap', function(){
-        $("#menu").toggle();
-    });
-                
-    $("#slidesContainer").bind('tap', function(){
-        $("#menu-bar").toggle();
-        $("#slider-footer").toggle();
-        $("#contenido").hide();
-    });
-                
-    var contador = 0;
-    $(".slide").each(function(){
-        contador++;
-        $(this).load('html/page'+contador+'.html');
-    });
+    $('#slideInner').css('height', slideInnerHeight);
+
+    $(document).bind('vmouseup',tools.evMouseUp);
+    $("#btn_left").bind('tap', tools.evLeft);
+    $("#btn_right").bind('tap', tools.evRight);
+    $("#nav-content li").bind('tap', tools.evContent);
+    $("#btn_home").bind('tap', tools.evHome);
+    $('#btn_menu').bind('tap', tools.showHideMenu);
+    $(document).bind('vmousedown', tools.updateScroll);
 });
---
+++ js/utils.js
@@ -0,0 +1,94 @@
+var slideWidth, slides, numberOfSlides, position, currentScroll;
+var slideInnerHeight = 511;
+var tools = {
+    manageControls : function (){
+        $("#btn_left").css('opacity','1');
+        $("#btn_right").css('opacity','1');
+                	
+        if (position == 0){
+            $("#btn_left").css('opacity','0.5');
+        }
+        else if (position >= numberOfSlides-1){
+            $("#btn_right").css('opacity','0.5');
+        }
+    },
+                
+    evRight :function (){
+        if (position < numberOfSlides-1){
+            position++;
+            tools.goToPage(position);
+        }
+    },
+                
+    evLeft :function (){
+        if (position > 0){
+            position--;
+            tools.goToPage(position);
+        }
+    },
+                
+    evContent : function (){
+        position = parseInt($(this).attr("rel"));
+        tools.goToPage(position);
+        $("#nav-content").hide();
+    },
+                
+    evHome : function (){
+        position = 0;
+        tools.goToPage(position);
+    },
+                    
+    evMouseUp : function (){
+        if ($("#slidesContainer").scrollLeft() > currentScroll){
+            var pos = ($("#slidesContainer").scrollLeft() + slideWidth)/slideWidth;
+        }else{
+            if ($("#slidesContainer").scrollLeft() < currentScroll){
+                var pos = ($("#slidesContainer").scrollLeft() - slideWidth)/slideWidth;
+            }
+            else
+                return false;
+        }
+        if (pos < 0)
+            pos = 0;
+
+        if (pos > numberOfSlides-1)
+            pos = numberOfSlides-1;
+
+        position = Math.round(pos);
+        tools.goToPage(position);
+             
+    },
+                
+    goToPage : function (page){
+        $(document).unbind('vmouseup');
+        $("#left").unbind('tap');
+        $("#right").unbind('tap');
+        $("#nav-content li").unbind('tap');
+        $("#btn_home").unbind('tap');
+        tools.manageControls();
+        $("#slidesContainer").animate({
+            scrollLeft:slideWidth*page
+        },500,'easeOutQuint',function(){
+            $(document).bind('vmouseup',tools.evMouseUp);
+            $("#left").bind('tap', tools.evLeft);
+            $("#right").bind('tap', tools.evRight);
+            $("#nav-content li").bind('tap', tools.evContent)
+            $("#btn_home").bind('tap', tools.evHome);
+        });
+    //        $("#num-slide").html(page+1);
+    },
+    showHideMenu : function(){
+        $("#menu_sections").toggle();
+    },
+    
+    loadSlides : function (){
+        var contador = 0;
+        $(".slide").each(function(){
+            contador++;
+            $(this).load('html/page'+contador+'.html');
+        });
+    },
+    updateScroll : function(){
+        currentScroll = $("#slidesContainer").scrollLeft();
+    }
+}
